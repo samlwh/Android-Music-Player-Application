1248be7889e0ba5d3e1df6c7b83b3e249dc13ca9
samlwh <samlwh@hotmail.com>
2018-08-22 15:38:15 +0800

    Adding Foreground Services, Messenger , Handlers and create separate process
    working on the processes and foreground Services.
    Running in the foreground on a separate Process than our Activity

--- app/src/main/java/com/samlwh/musicmachine/MainActivity.java
+++ app/src/main/java/com/samlwh/musicmachine/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,17 +19,23 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_SONG = "song";
     private boolean mBound = false;
-    private PlayerService mPlayerService;
     private Button mDownloadButton;
     private Button mPlayButton;
+    private Messenger mServiceMessenger;
+    private Messenger mActivityMessenger = new Messenger(new ActivityHandler(this));
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder binder) {
             mBound = true;
-            PlayerService.LocalBinder localBinder = (PlayerService.LocalBinder) binder;
-            mPlayerService = localBinder.getService();
-            if (mPlayerService.isPlaying()) {
-                mPlayButton.setText("Pause");
+            mServiceMessenger = new Messenger(binder);
+            Message message = Message.obtain();
+            message.arg1 = 2;
+            message.arg2 = 1;
+            message.replyTo = mActivityMessenger;
+            try {
+                mServiceMessenger.send(message);
+            } catch (RemoteException e) {
+                e.printStackTrace();
             }
         }
 
@@ -62,16 +70,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (mBound) {
-                    if (mPlayerService.isPlaying()) {
-                        mPlayerService.pause();
-                        mPlayButton.setText("Play");
-                    } else {
-                        mPlayerService.play();
-                        mPlayButton.setText("Pause");
+                    Intent intent = new Intent(MainActivity.this, PlayerService.class);
+                    startService(intent);
+                    Message message = Message.obtain();
+                    message.arg1 = 2;
+                    message.replyTo = mActivityMessenger;
+                    try {
+                        mServiceMessenger.send(message);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
                     }
                 }
             }
         });
+    }
+
+    public void changePlayButtonText(String text) {
+        mPlayButton.setText(text);
     }
 
     @Override
--- app/src/main/java/com/samlwh/musicmachine/PlayerService.java
+++ app/src/main/java/com/samlwh/musicmachine/PlayerService.java
@@ -1,17 +1,19 @@
 package com.samlwh.musicmachine;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Messenger;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 public class PlayerService extends Service {
     private static final String TAG = PlayerService.class.getSimpleName();
     private MediaPlayer mPlayer;
-    private IBinder mBinder = new LocalBinder();
+    public Messenger mMessenger = new Messenger(new PlayerHandler(this));
 
     @Override
     public void onCreate() {
@@ -19,11 +21,28 @@ public class PlayerService extends Service {
         mPlayer = MediaPlayer.create(this, R.raw.jingle);
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Notification.Builder notificationBuilder = new Notification.Builder(this);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        Notification notification = notificationBuilder.build();
+        startForeground(11, notification);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopSelf();
+                stopForeground(true);
+            }
+        });
+        return Service.START_NOT_STICKY;
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG, "onBind");
-        return mBinder;
+        return mMessenger.getBinder();
     }
 
     @Override
@@ -36,12 +55,6 @@ public class PlayerService extends Service {
     public void onDestroy() {
         Log.d(TAG, "onDestroy");
         mPlayer.release();
-    }
-
-    public class LocalBinder extends Binder {
-        public PlayerService getService() {
-            return PlayerService.this;
-        }
     }
 
     // Client Methods
