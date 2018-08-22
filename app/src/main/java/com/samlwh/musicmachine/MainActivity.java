4412e0c45a278bf90d84bea99d47f4896e0f92a9
samlwh <samlwh@hotmail.com>
2018-08-22 12:40:49 +0800

    Creating DownloadService to handle download task

--- app/src/main/java/com/samlwh/musicmachine/MainActivity.java
+++ app/src/main/java/com/samlwh/musicmachine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.samlwh.musicmachine;
 
+import android.content.Intent;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String KEY_SONG = "song";
     private Button mDownloadButton;
 
     @Override
@@ -17,11 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final DownloadThread thread = new DownloadThread();
-        thread.setName("DownloadThread");
-        thread.start();
-
         mDownloadButton = (Button) findViewById(R.id.downloadButton);
+
         mDownloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,9 +28,9 @@ public class MainActivity extends AppCompatActivity {
 
                 // Send Messages to Handler for processing
                 for (String song : Playlist.songs) {
-                    Message message = Message.obtain();
-                    message.obj = song;
-                    thread.mHandler.sendMessage(message);
+                    Intent intent = new Intent(MainActivity.this, DownloadService.class);
+                    intent.putExtra(KEY_SONG, song);
+                    startService(intent);
                 }
             }
         });
