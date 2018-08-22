65685a4fbe74c545bd40ae306f2bf6cb5436558a
samlwh <samlwh@hotmail.com>
2018-08-22 15:31:04 +0800

    Updating Layout and preview of the Git
    Updating Layout and preview of the Git

--- app/src/main/java/com/samlwh/musicmachine/Playlist.java
+++ app/src/main/java/com/samlwh/musicmachine/Playlist.java
@@ -1,5 +1,9 @@
 package com.samlwh.musicmachine;
 
 public class Playlist {
-    public static String[] songs = {"He’s the Greatest Dancer", "Cut the Cake", "The Groove Line", "Stayin’ Alive", "Give Up the Funk"};
+    public static String[] songs = {"Cardi B feat. Bad Bunny & J Balvin, \"I Like It\"",
+            "Drake, \"Nice for What\"",
+            "Ariana Grande, \"No Tears Left to Cry\"",
+            "Childish Gambino, \"This Is America\"",
+            "Kendrick Lamar & SZA, \"All the Stars\""};
 }
