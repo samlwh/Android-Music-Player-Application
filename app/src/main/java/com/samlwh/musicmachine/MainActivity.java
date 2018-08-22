d5ef5f59894f43ce1eb1aa806ad68564b47401e3
samlwh <samlwh@hotmail.com>
2018-08-22 14:37:25 +0800

    Adding IntentServices
    refactor our app by adding IntentServices

--- app/src/main/java/com/samlwh/musicmachine/MainActivity.java
+++ app/src/main/java/com/samlwh/musicmachine/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // Send Messages to Handler for processing
                 for (String song : Playlist.songs) {
-                    Intent intent = new Intent(MainActivity.this, DownloadService.class);
+                    Intent intent = new Intent(MainActivity.this, DownloadIntentService.class);
                     intent.putExtra(KEY_SONG, song);
                     startService(intent);
                 }
