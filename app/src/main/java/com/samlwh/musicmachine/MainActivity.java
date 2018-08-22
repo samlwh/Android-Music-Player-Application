ba405a2ea1c80bf00cd7c3ad9e1cb764919ebf08
samlwh <samlwh@hotmail.com>
2018-08-22 14:58:11 +0800

    Binding Initiated
    setup the app to start the binding process and some update for service connections

--- app/src/main/java/com/samlwh/musicmachine/MainActivity.java
+++ app/src/main/java/com/samlwh/musicmachine/MainActivity.java
@@ -1,6 +1,10 @@
 package com.samlwh.musicmachine;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,8 +16,20 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_SONG = "song";
+    private boolean mBound = false;
     private Button mDownloadButton;
     private Button mPlayButton;
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +58,21 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, PlayerService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
     }
 }
